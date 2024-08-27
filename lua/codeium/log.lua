b148b1a9e05e7fa85568251c04a073ad7eeaf6c9
bstaint <bstaint@gmail.com>
2024-08-27 13:54:46 +0800

    Fix log path & Update version.

--- lua/codeium/log.lua
+++ lua/codeium/log.lua
@@ -1,6 +1,7 @@
 local p_debug = vim.env.DEBUG_CODEIUM
 
 return require("plenary.log").new({
-	plugin = "codeium/codeium.log",
+	plugin = "codeium/codeium",
+	use_console = false,
 	level = p_debug or "info",
 })
